8f961c9a24f6c3bc6acf73a91b6fbcb068cdaecd
DillonMiller618 <146030102+DillonMiller618@users.noreply.github.com>
2024-04-26 18:33:01 +0000

    animalquiz finished

--- Snippets/April/Apr. 25/animalquiz.py
+++ Snippets/April/Apr. 25/animalquiz.py
@@ -4,28 +4,70 @@ import time
 print("Welcome to the animal quiz. Inputs are case and space insensitive.")
 
 
-def questionaire(prompt, answer):
+def openQuestion(prompt, answer):
     global score
-    attempts = 0
-    while attempts != 3:
+    attemptsRemaining = 3
+    while attemptsRemaining != 0:
         if input(prompt).strip().lower() == answer:
-            score += 1
+            score += attemptsRemaining
             print(f"Correct!\nYour score is {score}!")
+            break
         else:
-            attempts += 1
-            print(f"Incorrect!\nTry again! You have {3-attempts} attempts left.")
+            attemptsRemaining -= 1
+            print(f"Incorrect!\nTry again! You have {attemptsRemaining} attempts left.")
             
-    if attempts == 3:
-        score -= 1
-        print(f"Too many guesses!\nYour score is {score}")
+    if attemptsRemaining == 0:
+        score -= 2
+        print(f"Too many guesses!\nYour score is {score}!")
     return score
+
+def multipleChoice(prompt, A, B, C, D, correctAnswer):
+    #only one attempt b/c multiple choice is more forgiving, 3 points rewarded like normal questions. Lists are just for fancy animations.
+    #(basically the entire time module is used for making the usage of the program cooler)
+    answers = [A, B, C, D]
+    headers = ["A: ", "B: ", "C: ", "D: "]
+    global score
+    print(prompt)
+    for i in range(len(answers)):
+        time.sleep(.5)
+        print(f"{headers[i]}{answers[i]}")
+        i += 1
+    time.sleep(1.5)
+    userInput = input("What's your answer? ").strip().lower()
+    if userInput == correctAnswer:
+        score += 3
+        print(f"Correct!\nYour score is {score}!")
+    else:
+        score -= 1
+        print(f"Incorrect!\nYour score is {score}!")
+    return score
+
+def trueFalse(prompt, answer):
+    global score
+    print(f"True or false: {prompt}")
+    time.sleep(1.5)
+    userInput = input("What's your answer? ").strip().lower()
+    if userInput == answer:
+        score += 2
+        print(f"Correct!\nYour score is {score}!")
+    else:
+        score -= 1
+        print(f"Incorrect!\nYour score is {score}!")
+    return score
+
 
 score = 0
 
 def main():
-    questionaire("What has webbed feet, quacks like it's always gossiping, and enjoys swimming? ", "duck")
-    questionaire("What is massive, has a trunk but no keys, and is afraid of mice? ", "elephant")
-    questionaire("What animal has stripes, can go from 0-60 in under 10 seconds, and is the mascot of a corn puffs brand? ", "cheeta")
+    trueFalse("Cheetas roar, like all big cats.", "false")
+    openQuestion("What has webbed feet, quacks like it's always gossiping, and enjoys swimming? ", "duck")
+    trueFalse("Sea otters have favorite rocks they use to crack open their food with.", "true")
+    multipleChoice("What is super spikey, lives in the ocean, and is similar to street urchins? ", "Hedgehog", "Sea Urchin", "Sea Anenome", "Rat", "b")
+    openQuestion("What is massive, has a trunk but no keys, and is afraid of mice? ", "elephant")
+    trueFalse("Dogs sneeze whenever they're playing to indicate that they aren't having fun and want to stop.", "false")
+    openQuestion("What animal has stripes, can go from 0-60 in under 10 seconds, and is the mascot of a corn puffs brand? ", "cheeta")
+    multipleChoice("What do you call a group of owls?", "Troop", "Clutter", "Smack", "Paraliment", "d")
+    print(f"You finished the quiz!\nYour final score was {score}/21 questions correct.")
 
 
 
