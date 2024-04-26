8f69cd398fcda590d5cc859edf58c750d74fc98f
DillonMiller618 <146030102+DillonMiller618@users.noreply.github.com>
2024-04-26 19:14:06 +0000

    comments and update readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Solutions to questions and assignments I had during my NCSSM Connect Course, Honors Introduction to Computer Science and Computational thinking, with Mr. Robinson.
 
 Not intended to be a cheatsheet, just a way for me to submit my assignments and show my work.
+All was coded using GitHub codespaces, and pushed to the main branch on repo, in case any other newbies want to replicate the sweet setup I had for this class.
+Most coding took place in April/May, with a lot of challenges. I also partook in some PyGame shenanigans, but that never went anywhere unforntunately, but I resolve to do it one day.
--- Snippets/April/Apr. 19/spacejunk.py
+++ Snippets/April/Apr. 19/spacejunk.py
@@ -5,8 +5,7 @@ from inputimeout import inputimeout, TimeoutOccurred #idk if ill use this
 
 
 
-
-completed = False
+#frames and general vars that are used for multiple functions, so I put them here
 spaceFrame1 = """                                 
         _____                                                               +
     ,-:` \;',`'-,                       *                       
@@ -33,19 +32,44 @@ Do you wish to fly around Earth to avoid the largest cluster of space debris, or
 avoid the debris, so you can get a time bonus on your mission?
 Enter either 1 to fly around Earth, or 2 to fly straight into the debris cloud. """
 gameOver = pyfiglet.figlet_format("GAME OVER",font= "isometric3")
-gameWon = "you won"
+gameWon = """
+           .       .                   .       .      .     .      .
+          .    .         .    .            .     ______
+      .           .             .               ////////
+                .    .   ________   .  .      /////////     .    .
+           .            |.____.  /\        ./////////    .
+    .                 .//      \/  |\     /////////
+       .       .    .//          \ |  \ /////////       .     .   .
+                    ||.    .    .| |  ///////// .     .
+     .    .         ||           | |//`,/////                .
+             .       \\        ./ //  /  \/   .
+  .                    \\.___./ //\` '   ,_\     .     .
+          .           .     \ //////\ , /   \                 .    .
+                       .    ///////// \|  '  |    .
+      .        .          ///////// .   \ _ /          .
+                        /////////                              .
+                 .   ./////////     .     .
+         .           --------   .                  ..             .
+  .               .        .         .                       .
+                        ________________________
+____________------------                        -------------_________"""
+
 
 def intro():
-    intro = pyfiglet.figlet_format("Space Junk",font= "isometric3")
-    print(f"{intro}\n\nA Choose Your Own Adventure game, where you propel yourself off of different space objects to reach you endgoalreplacelater\nPress Enter to begin. ", end="")
-    start = input("")
-    if start != "":
-        exit()
-    else:
-        return True
-
+    #yes, I am aware that not entering enter just spams the terminal. Don't try it at home kids.
+    #begins the shenenagins through introducing the game, and asking for the user to press enter. Once enter is pressed, it returns true, used in main().
+    while True:
+        intro = pyfiglet.figlet_format("Space Junk",font= "isometric3")
+        print(f"{intro}\n\nA Choose Your Own Adventure game, where you dodge space objects or tightly manage your fuel to reach planet Earth before it runs out or you collide with space junk.\nPress Enter to begin. ", end="")
+        start = input("")
+        if start != "":
+            continue
+        else:
+            return True
 
 def debrisPath(sequence, timer):
+    #no, this path didn't take me 4 days because i was trying to thread a timer and input at the same time but don't know how to do threads
+    #asks for the user to input a sequence in less than the timer, using inputimeout module to determine that. if the user inputs said input, move on to the next one in the path determined in main()
     while True:
         print(f"Input --> {sequence} <-- in the next {timer} seconds to dodge the space junk.")
         userInput = inputimeout(prompt="", timeout=timer)
@@ -59,67 +83,37 @@ def debrisPath(sequence, timer):
             print(gameOver)
             break
         
-
-    
 def pathAround():
+    #procrastination is the devil's incarnation in all humans. (aka this took forever for me to get to)
+    #idk i havent gotten here yet
     print("You have chosen to fly around Earth. However, your ship's treasurer has miscalculated the amount of fuel you have left, and \nwithout some drastic measures, you won't be able to make it to Earth in time. Get ready to make some difficult choices.")
     time.sleep(5)
     userInput = input("")
 
-
-
-
-
-
-    
 def main():
+    #where the magic happens, and my if statement count doubles
+    #asks the user for a choice between two paths, and commences the path. Each path has 3 decisions/inputs, and if all 3 are done within tolerance, gameWon screen is shown. else, gameOver is shown.
     if intro() == True: 
-        choice1 = int(input(f"{spaceFrame1}\n{spaceText1}"))
-        if choice1 == 2:
-            print("You have chosen the debris path. Get ready to dodge space junk.")
-            time.sleep(4)
-            if debrisPath("low", 4) == True:
-                print("You continue your flight rapidly towards the Earth, but another piece of space junk found itself in your path. \nGET READY TO DODGE!")
-                time.sleep(5)
-                if debrisPath("up", 3) == True:
-                    print("You're almost to the landing pad, but yet another piece of space junk crosses your path. It's now or never: TIME TO DODGE!")
+        while True:
+            choice1 = input(f"{spaceFrame1}\n{spaceText1}")
+            if choice1 == "2":
+                print("You have chosen the debris path. Get ready to dodge space junk.")
+                time.sleep(4)
+                if debrisPath("low", 4) == True:
+                    print("You continue your flight rapidly towards the Earth, but another piece of space junk found itself in your path. \nGET READY TO DODGE!")
                     time.sleep(5)
-                    if debrisPath("right", 5) == True:
-                        print(gameWon)
-        elif choice1 == 1:
-            pathAround()
-            
-        
-        else:
-            print("Try again.")
+                    if debrisPath("up", 3) == True:
+                        print("You're almost to the landing pad, but yet another piece of space junk crosses your path. It's now or never: TIME TO DODGE!")
+                        time.sleep(5)
+                        if debrisPath("right", 5) == True:
+                            print(f"You avoided all of the debris, and no major damage was dealt to your ship.\nFine work.\n{gameWon}")
+                            break
+            elif choice1 == "1":
+                pathAround()
 
-
-            
-
-
-            
-
+            else:
+                print("Try again.")
     
 main()
 
 #some ascii art has been copied from https://www.asciiart.eu/space/
-
-
-
-"""
-def debrisPath(sequence, timer):
-    start_time = time.time()  # Record the start time
-    while True:
-        remaining_time = timer - (time.time() - start_time)
-        if remaining_time <= 0:
-            print(f"\n{gameOver}")
-            break
-        try:
-            user_input = input(f"You have {round(remaining_time, 1)} seconds until a large piece of debris collides with your ship!\nEnter {sequence} to avoid the debris: ", end="\r")
-            if user_input == sequence:
-                return True
-            else:
-                print(gameOver)
-                break
-        except TimeoutOccurred:
-            pass  # Continue the loop if no input is received within the timeout period"""
