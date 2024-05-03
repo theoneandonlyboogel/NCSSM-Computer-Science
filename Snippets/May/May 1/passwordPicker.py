0b3b4bd030fe3e8745001d129e82520511a25e45
DillonMiller618 <146030102+DillonMiller618@users.noreply.github.com>
2024-05-03 15:13:30 +0000

    Co-authored-by: Robinson, Charles <charles.robinson@ncssm.edu>

--- Snippets/May/May 1/passwordPicker.py
+++ Snippets/May/May 1/passwordPicker.py
@@ -1,5 +1,11 @@
+"""
+challenge 1: give your program a clear beginning and end✓
+challenge 2: add more words to add complexity
+challenge 3: change the code so the program will create and display at three passwords at once
+challenge 4: make it longer- by adding another word into each password"""
 import random
 import string
+import pyfiglet
 
 order = []
 for i in range(1,4):
@@ -36,13 +42,33 @@ nouns = [
     "rehabilitation",
     "macroeconomics"
 ]
-adjective = random.choice(adjectives)
-noun = random.choice(nouns)
-number = random.randrange(0, 100000)
-specialChar = random.choice(string.punctuation)
 
+print(pyfiglet.figlet_format("Password Picker",font= "chunky"))
+while True:
+    qty = int(input("How many passwords? "))
+    for i in range(0, qty):
+        adjective = random.choice(adjectives)
+        noun = random.choice(nouns)
+        number = random.randrange(0, 100000)
+        specialChar = random.choice(string.punctuation)
 
+        adjNoun = adjective + "_" + noun
 
-
-print(f"{adjective}\n{noun}\n{number}\n{specialChar}")
-
+        order = []
+        while len(order) < 3:
+            t = random.randint(1, 3)
+            if t not in order:
+               order.append(t)
+               i+1
+            else:
+               continue
+            fullString = "".join(str(order)).replace("[", "").replace("]", "").replace(",", "")
+        password = str(fullString.replace("2", adjNoun).replace("1", specialChar).replace("3", str(number)).replace(" ", ""))
+        print(password)
+        i += 1
+    userInput = input("Would you like another password? Type Y/n ").lower()
+    if userInput == "n":
+        print(pyfiglet.figlet_format("Cya!",font= "chunky"))
+        break
+    if userInput == "y":
+        continue
