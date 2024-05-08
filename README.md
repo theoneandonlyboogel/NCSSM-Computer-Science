c01b0f2dc09dd423923f1028ed46c0dd131d46da
DillonMiller618 <146030102+DillonMiller618@users.noreply.github.com>
2024-05-08 15:05:54 +0000

    5/8/24 Class

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-Solutions to questions and assignments I had during my NCSSM Connect Course, Honors Introduction to Computer Science and Computational thinking, with Mr. Robinson.
+Solutions to questions and assignments I had during my NCSSM Coding courses, Honors Introduction to Computer Science and Computational thinking, with Mr. Robinson, and AP Computer Science Principles, with ????
 
 Not intended to be a cheatsheet, just a way for me to submit my assignments and show my work.
-All was coded using GitHub codespaces, and pushed to the main branch on repo, in case any other newbies want to replicate the sweet setup I had for this class.
-Most coding took place in April/May, with a lot of challenges. I also partook in some PyGame shenanigans, but that never went anywhere unforntunately, but I resolve to do it one day.
+All was coded using GitHub codespaces, and pushed to the main branch on repo, in case any other newbies want to replicate the setup I had for these classes.
+Most coding took place in April/May in CS and CT, with a lot of challenges.
--- Snippets/May/May 6/fibonacci.py
+++ Snippets/May/May 6/fibonacci.py
@@ -1,18 +1,52 @@
+"""
+#iterative with for loop
+def printFib(length):
+    first = 0
+    second = 1
+
+    print(first, second, sep="+")
+    for i in range(2, length):
+        temp = second
+        second = first + second
+        first = temp
+        print(second)
+        i+=2
+print("Fib Series")
+printFib(20)
+"""
+"""
+#Mr. Robinson's approach
+def fibonacci(n):
+    if n <= 1:
+        return n
+    else:
+        fib = [0, 1]
+        for i in range(2, n + 1):
+            fib.append(fib[i - 1] + fib[i - 2])
+            return fib[n]
+n = int(input("Enter the value of n: "))
+print("The nth number in the sequence is: {fibonacci(n)})
+"""
 fibs = []
+#recursive version, with challenge
 def fibo(first, second, length):
     if length == 0:
         return
     
+    #third is effectively a temp var
     third = first+second
     print(third)
     fibs.append(third)
     #recursively call the function (updating the values)
     fibo(second, first + second, length -1)
-i = int(input("How many sequences? "))
-sequenceTF = bool(input("Do you want a specific sequence? Press enter for No, any key and enter for yes. "))
 
+#asking for how many sequences
+i = int(input("How many sequences? "))
+#True False if you want a specific sequence at the end
+sequenceTF = bool(input("Do you want a specific sequence? Press enter for No, any key for yes. "))
+#if sequenceTF is true, aka they inputed anything, ask for which sequence they want. Else, just print as normal.
 if sequenceTF == True:
     sequenceNum = int(input("Which sequence do you want? "))
 fibo(0, 1, i)
 if bool(sequenceNum) == True:
-    print(f"-----------------------------------------\n{fibs[sequenceNum-1]}")
+    print(f"Specific sequence:\n{fibs[sequenceNum-1]}")
