9becf29c6567ee15ab259cee0eafdff892446a29
DillonMiller618 <146030102+DillonMiller618@users.noreply.github.com>
2024-11-12 19:16:09 +0000

    Notes and Final Project Work

---
+++ Creative Design For The Web/Notes/JS Notes/jsnotes0/functions.js
@@ -0,0 +1,17 @@
+//Most basic JavaScript functionality:
+console.log("Hey There!");
+//Logs the message (in string format in this case) in the console
+//Calls the method of Class console, log, which outputs the arguments in the web console
+//Variable practice
+let studentName = "Dillon"; //string
+let studentGrade = 100; //int
+let studentIsLearning = true; //bool
+console.log("Student Name:", studentName);
+console.log("Student Grade:", studentGrade);
+console.log("Student is Learning: :", studentIsLearning);
+
+let message = "Hey there, how's it going?";
+console.log("Message length: ", message.length);
+console.log("Uppercase:", message.toUpperCase);
+console.log("Lowercase", message.toLowerCase);
+
